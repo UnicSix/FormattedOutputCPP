69f8e97fd58ad35c63c21bb34040e970116f2bdf
noookia8110 <matchman7980@gmail.com>
2023-08-01 18:31:52 +0800

    exclude unimportant info

--- FormattedOutputCPP/FormatOutput.h
+++ FormattedOutputCPP/FormatOutput.h
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <string.h>
 #include <string>
@@ -11,9 +11,9 @@ void ReadFile()
 {
     int line_index=0;
     string target = ", ";
-    // string file_dir = "@AutomationLog.txt";
+    string file_dir = "@Automation.txt";
     // string file_dir = "datagridview.txt";
-    string file_dir = "listbox.txt";
+    // string file_dir = "listbox.txt";
     string readline;
     string outputLines="";
     string test_str = "ControlType: PaneControl    ClassName: #32769    AutomationId:     Rect: (0,0,1920,1080)[1920x1080]    Name: '桌面 1'    Handle: 0x10010(65552)    Depth: 0    SupportedPattern: LegacyIAccessiblePattern";
@@ -21,7 +21,9 @@ void ReadFile()
     ofstream outputFile("outputFile.txt", 'w');
     regex rect_pattern("(Rect: )\\(((-*\\d+,){3}-*\\d+)\\)");
     regex name_pattern("(Name: )('([^']*)')");
-    regex value_pattern("(ValuePattern.Value: )'([^']*)'");
+    regex value_pattern("(ValuePattern.Value: )('([^']*)')");
+    regex pass_pattern("CustomControl|HeaderControl|MenuBarControl|MenuItemControl|ButtonControl");
+    // regex pass_pattern("資料列|CustomControl");
     if(inputFile.is_open())
     {
         while(getline(inputFile, readline))
@@ -29,33 +31,43 @@ void ReadFile()
             smatch rect_match;
             smatch name_match;
             smatch value_match;
-            if(regex_search(readline, rect_match, rect_pattern))
+            smatch pass_match;
+            if(regex_search(readline, pass_match, pass_pattern))
             {
-                line_index++;
-                outputLines+=to_string(line_index);
-                outputLines+="@";
-                outputLines+=rect_match[2];
-            }
-            if(regex_search(readline, value_match, value_pattern))
-            {
-                outputLines += value_match[0];
+                cout << pass_match[0] << endl;
+                continue;
             }
             else
             {
-                if(regex_search(readline, name_match, name_pattern))
-                {
-                    outputLines += name_match[0];
-                }
-            }
-            Replace_all(outputLines, ",", "@");
-            Replace_all(outputLines, "'", "@");
-            if(outputLines != "")
-            {
-                outputLines.pop_back();
-                outputFile << outputLines << endl;
+                if(regex_search(readline, rect_match, rect_pattern))
+                    {
+                        line_index++;
+                        outputLines+=to_string(line_index);
+                        outputLines+="@";
+                        outputLines+=rect_match[2];
+                    }
+                    if(regex_search(readline, value_match, value_pattern))
+                    {
+                        outputLines += value_match[2];
+                    }
+                    else
+                    {
+                        if(regex_search(readline, name_match, name_pattern))
+                        {
+                            outputLines += name_match[2];
+                        }
+                    }
+                    Replace_all(outputLines, ",", "@");
+                    Replace_all(outputLines, "'", "@");
+                    if(outputLines != "")
+                    {
+                        outputLines.pop_back();
+                        outputFile << outputLines << endl;
+                    }
             }
             outputLines = "";
         }
+        inputFile.close();
     }
     else
     {
